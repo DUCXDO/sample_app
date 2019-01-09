e4897fc90564dfed9b86ef2e8bc7a143974570ee
NguyenMinhDuc <nguyenminhduc9197@gmail.com>
2019-01-09 13:18:47 +0700

    Chapter 9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,42 @@
 class User < ApplicationRecord
-  before_save{self.email = email.downcase}
-  validates :name,  presence: true, length: {maximum: Settings.user_name_max}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_reader :remember_token
+  validates :name,  presence: true, length: {maximum: Settings.name_max}
   validates :email, presence: true, length: {maximum: Settings.email_max},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  has_secure_password
   validates :password, presence: true, length: {minimum: Settings.password_min}
+  before_save :email_downcase
+  has_secure_password
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  private
+
+  def email_downcase
+    email.downcase!
+  end
 end
